6ac60730cbb64a9e43417760410e00a43b318b95
Qi Cao <alanqicao@gmail.com>
2021-09-21 21:37:49 -0600

    ce

---
+++ 2420_CLASS_EXERCISE/src/ceRecursion/Recursion.java
@@ -0,0 +1,34 @@
+package ceRecursion;
+
+public class Recursion {
+	
+	private static void hailstone (int seed) {
+		
+		if(seed < 0) {
+			throw new IllegalArgumentException("Seed need to be a positive number");
+		}
+		
+		//first number is seed provided by client	
+		System.out.print(seed+" ");
+		
+		if(seed == 1) {
+			System.out.println(seed);
+		}	
+		else if(seed % 2 == 0) {
+			hailstone(seed/2);			
+		}else {
+			hailstone(seed * 3 +1);		
+		}
+
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.printf("Hailstone numbers\n-----------------\n");
+		hailstone(3);
+		hailstone(16);
+		hailstone(17);
+		hailstone(24);
+	}
+	
+}
