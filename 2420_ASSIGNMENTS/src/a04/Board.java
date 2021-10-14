08db979d527ece66fa5b2d3b14b7e8cc306568cc
alanqicao <alanqicao@gmail.com>
2021-10-14 17:00:01 -0600

    a04

--- 2420_ASSIGNMENTS/src/a04/Board.java
+++ 2420_ASSIGNMENTS/src/a04/Board.java
@@ -3,6 +3,8 @@
  */
 package a04;
 
+import java.util.Arrays;
+
 /**
  * @author Qi Cao
  *
@@ -11,8 +13,13 @@ public class Board {
 	
 	private int N;
 	//one dimansion array
-	private int [] oneDimenArray= new int[N*N];
-	
+	private int [] oneDimenArray;
+	//two dimansion array
+	private int [][] twoDimenArray;
+	//target board
+	private Board targetBoard;
+	//empty spot
+	private int emptySpot;
 	
 	/**
 	 * construct a board from an N-by-N array of blocks
@@ -25,13 +32,28 @@ public class Board {
 		this.N = blocks.length;
 		
 		//convert two dimansion to one dimansion
-		int idx= 0;
+		this.oneDimenArray = new int[N*N];
+		
+		int counter= 0;
+		
 		for(int i = 0; i<blocks.length;i++) {
 			for(int j = 0 ; j<blocks[i].length;j++) {
-				oneDimenArray[idx++]= blocks[i][j];
+				oneDimenArray[counter++]= blocks[i][j];
 			}
 		}
+		
+		//copy two diman array blocks to class field
+		this.twoDimenArray = new int [blocks.length][blocks.length];
+		for(int i = 0; i<blocks.length;i++) {
+			for(int j =0; j<blocks.length; j++) {
+				this.twoDimenArray[i][j] = blocks[i][j];
+			}
+		}
+		
+		
 	}          
+
+	
 
 	/**
 	 * board size N
@@ -39,7 +61,7 @@ public class Board {
 	 */
 	public int size() {
 		
-		return N*N;
+		return N;
 	}                   
 	
 	/**
@@ -48,7 +70,21 @@ public class Board {
 	 */
 	public int hamming() {
 		
-		return 0;
+		//Hamming move
+		int totalHamming = 0;
+		
+		for(int i = 1; i<oneDimenArray.length+1; i++) {
+			
+			if(oneDimenArray[i-1] == 0) {
+				
+			}
+			else if(oneDimenArray[i-1]!=i) {
+				totalHamming++;
+			}
+								
+		}
+		
+		return totalHamming;
 		
 	} 
 	
@@ -57,6 +93,26 @@ public class Board {
 	 * @return
 	 */
 	public int manhattan() {
+		
+		int manDistance = 0;
+		
+		int [][] goalBoard = goalBoard(N);
+		
+		for(int row = 0; row < N; row++) {
+			
+			for(int column =0; column < N; column++) {
+				
+				if(twoDimenArray[row][column] == 0 ) {
+					
+					manDistance=manDistance;
+					
+				}else {
+					//  psudo code
+					manDistance += Math.abs(twoDimenArray[row] - goalBoard[row]);
+					manDistance += Math.abs(twoDimenArray[column] - goalBoard[column]);
+				}
+			}
+		}
 		
 		return 0;
 	}
@@ -68,6 +124,27 @@ public class Board {
 	public boolean isGoal() {
 		
 		return false;
+	}
+	
+	//generate goal board
+	private int [][] goalBoard(int n){
+		
+		int [][] goalBoard = new int[n][n];
+		
+		int counter = 1;
+		
+		for(int row = 0; row <n; row++) {
+			for(int column = 0; column<n; column ++) {
+				
+				
+				goalBoard[row][column] = counter++;
+				
+			}
+		}
+		
+		goalBoard[n-1][n-1] =0;
+		
+		return goalBoard;
 	}
 	
 	/**
@@ -106,16 +183,44 @@ public class Board {
 	 */
 	public String toString() {
 		
-		return null;
+		 StringBuilder s = new StringBuilder();
+		    s.append(N + "\n");
+		    for (int i = 0; i < N; i++) {
+		        for (int j = 0; j < N; j++) {
+		            s.append(String.format("%2d ", twoDimenArray[i][j]));
+		        }
+		        s.append("\n");
+		    }
+		    return s.toString();
 		
 	}
 
 	/**
+	 * Testing
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int [] testArray1D = {8,1,3,4,0,2,7,6,5};
+		int [][] testArray2D = new int[3][3];
+		
+		int counter = 0;
+		
+		for(int i= 0; i<testArray2D.length;i++) {
+			for(int j = 0; j<testArray2D[i].length;j++) {
+				testArray2D[i][j] = testArray1D[counter++];
+			}
+		}
+		
+		Board testBorad = new Board(testArray2D);
+		
+		//testing Hamming distances expect 5
+		System.out.println("Hamming distances: "+testBorad.hamming());
+		//old twoD array
+		System.out.println("Old board: "+Arrays.deepToString(testArray2D));
+		//testing generate goal board
+		System.out.println("generate goal board: "+Arrays.deepToString(testBorad.goalBoard(3)));
+		
 	}
 
 }
--- 2420_CLASS_EXERCISE/src/ceMail/DemoHeap.java
+++ 2420_CLASS_EXERCISE/src/ceMail/DemoHeap.java
@@ -1,32 +1,36 @@
 package ceMail;
 
+import edu.princeton.cs.algs4.MaxPQ;
 
-import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
 public class DemoHeap {
 
-		
-	
 	public static void main(String[] args) {
-		
-		
-		MinPQ<Mail> mails = new MinPQ<>();
-		
-		
-		
-		for(int i = 0; i<25;i++) {
-			
+
+		MaxPQ<Mail> mails = new MaxPQ<>();
+
+		StdOut.println("25 random mail objects:");
+		StdOut.println("-----------------------");
+
+		for (int i = 0; i < 25; i++) {
+
 			Mail mail = new Mail();
-			
+			StdOut.println(mail);
 			mails.insert(mail);
+
 		}
-		
-		
-		
-		
-		
-		
-		
+
+		StdOut.printf("\nMail Delivery:\n");
+		StdOut.println("--------------");
+	
+
+		while (!mails.isEmpty()) {
+
+			StdOut.println(mails.delMax());
+
+		}
+
 	}
 
 }
